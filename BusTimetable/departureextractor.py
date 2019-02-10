a19dc779279b028c035ef674c8af3b4520f919d8
Robin Wyss <me@robinwyss.com>
2019-02-10 22:47:25 +0100

    extract timetable

--- BusTimetable/departureextractor.py
+++ BusTimetable/departureextractor.py
@@ -10,9 +10,14 @@ class DepartureTimeExtractor:
         return [int(i) for i in next_departures.split('min') if i != '']
 
     def timetable(self):
-        #self.soup.find_all('table')
         tablerows = self.soup.find_all('table')[1].find_all('tr', class_=re.compile("even|odd"))
-        for row in tablerows:
-            print(row.get_text())
-        return tablerows
+        # strip the last four rows, as they don't contain times
+        usedtablerows = tablerows[:len(tablerows)-4]  
+        timetable = []
+        for row in usedtablerows:
+            cells = row.find_all("td")
+            hour = cells[0].get_text().strip()
+            minutes = cells[1].get_text().strip().split(" ")
+            timetable.append((hour, minutes))
+        return timetable
         
