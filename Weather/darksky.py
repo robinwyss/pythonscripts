e1e46685d390396c1f423c2ee76f69e03f3f231e
Robin Wyss <me@robinwyss.com>
2020-02-12 22:16:13 +0100

    reduced information retrieved

--- Weather/darksky.py
+++ Weather/darksky.py
@@ -8,6 +8,6 @@ forecast_request = "https://api.darksky.net/forecast/"
 def get_forecast(api_key, lat, lng):
     url = forecast_request + api_key + "/" + lat + "," + lng
     print(url)
-    params = {"units": "si", "exclude": "minutely,daily,alerts,flags"}
+    params = {"units": "si", "exclude": "minutely,alerts,flags"}
     r = requests.get(url, params)
     return r.json()
